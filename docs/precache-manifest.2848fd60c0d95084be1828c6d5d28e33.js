9b395bade8e99a5febdb027363c524e77adb22ce
Michael Slain <michaelslain@Lizas-Air.attlocal.net>
2020-05-13 22:21:18 -0700

    Edited README and fixed docs folder

---
+++ docs/precache-manifest.2848fd60c0d95084be1828c6d5d28e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3ae90e6c098dfe483f688bd669c6f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29a7dca1b8c95a4b271d",
+    "url": "./static/css/main.b857ded1.chunk.css"
+  },
+  {
+    "revision": "7fff7ce6a8f6f6c5a6c9",
+    "url": "./static/js/2.021d4ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.021d4ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a7dca1b8c95a4b271d",
+    "url": "./static/js/main.0a7390fe.chunk.js"
+  },
+  {
+    "revision": "0b3121e3e583953d7a3e",
+    "url": "./static/js/runtime-main.70013e01.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea3695e88e173b0e9bfcb897934e91af.js"
+  "./precache-manifest.2848fd60c0d95084be1828c6d5d28e33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
